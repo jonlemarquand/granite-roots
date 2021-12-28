7c74672475b0cff1a1bf73ca45626187a0ce5deb
Jon Le Marquand <jon.lemarquand@purrgroup.com>
2021-12-28 20:36:46 +0000

    Update front page

--- web/app/themes/paws/front-page.php
+++ web/app/themes/paws/front-page.php
@@ -10,15 +10,20 @@
 ?>
 <?php get_header(); ?>
 <div class="container front-page">
-	<h1>Granite<span>Roots</span></h1>
+	<h1>Granite<span class="front-page__logo--span">Roots</span></h1>
 	<div class="subtitle"><?php echo $subtitle ?></div>
-	<div class="front-page-main-post">
-		<?php 
+	<section>
+	<?php 
+			$permalink = get_permalink($posts[0]->ID);
 			$post_thumbnail = wp_get_attachment_url( get_post_thumbnail_id($posts[0]->ID), 'thumbnail' );
-		?>
+			$post_date = get_the_date(null, $posts[0]->ID);
+	?>
+	<a href="<?php echo $permalink ?>" class="front-page-main-post">
 				<img class="front-page-main-post__image" src="<?php echo $post_thumbnail?>" />
+				<p class="subtitle"><?php echo $post_date ?></p>
 				<h2><?php echo $posts[0]->post_title ?></h2>
-	</div>
+				<p>As I walk around the paths through the healthland I take delight in the colours around me.</p>
+	</a>
 	<div class="post-grid-3">
 	<?php for($i = 1; $i < count($posts); $i++)
 	{ 
@@ -26,21 +31,20 @@
 		$post_date = get_the_date(null, $posts[$i]->ID);
 		$permalink = get_permalink($posts[$i]->ID);
 						?>
-						<a class="frontpage-post" href="<?php echo $permalink ?>">
+						<a class="front-page-post" href="<?php echo $permalink ?>">
 						<?php if($post_thumbnail) { ?>
-							<img class="frontpage-post__image" src="<?php echo $post_thumbnail ?>" />
+							<img class="front-page-post__image" src="<?php echo $post_thumbnail ?>" />
 						<?php } else { ?>
-							<div class="frontpage-post__image--none frontpage-post__image">GR</div>
+							<div class="front-page-post__image--none front-page-post__image">GR</div>
 						<?php } ?>
-							<div>
-								<p class="subtitle"><?php echo $post_date ?></p>
-								<h3><?php echo $posts[$i]->post_title ?></h3>
-								<p>As I walk around the paths through the healthland I take delight in the colours around me.</p>
-							</div>
+							<p class="subtitle"><?php echo $post_date ?></p>
+							<h3><?php echo $posts[$i]->post_title ?></h3>
+							<p>As I walk around the paths through the healthland I take delight in the colours around me.</p>
 						</a>
 						<?php
 					}
 			?>
 	</div>
+	</section>
 </div>
 <?php get_footer(); ?>
