74b7170e6f71cc70500bccfda82ddfa853ac1521
Jon Le Marquand <jon.lemarquand@purrgroup.com>
2021-12-28 16:21:13 +0000

    Add related posts option

---
+++ web/app/themes/paws/front-page.php
@@ -0,0 +1,8 @@
+<?php 
+	$title = get_bloginfo();
+	$subtitle = get_bloginfo('description');
+?>
+<?php get_header(); ?>
+	<h1>Granite<span>Roots</span></h1>
+	<div class="subtitle"><?php echo $subtitle ?></div>
+<?php get_footer(); ?>
--- web/app/themes/paws/single.php
+++ web/app/themes/paws/single.php
@@ -7,6 +7,29 @@
 		<p class="subtitle"><?php the_date(); ?></p>
 		<h1><?php the_title(); ?></h1>
 		<?php the_content(); ?>
-	</div>
+		<?php 
+			$related_content = get_field('related_posts');
+			if($related_content) { 
+		?>
+				<h2>Related Posts</h2>
+			<?php 
+				foreach($related_content as $related_post) { ?>					
+					<?php
+						$post_thumbnail = wp_get_attachment_url( get_post_thumbnail_id($related_post->post_id), 'thumbnail' );
+						$post_date = get_the_date(null, $related_post->post_id);
+					?>
+					<a class="related-post" href="<?php echo $related_post->guid ?>">
+						<img class="related-post__image" src="<?php echo $post_thumbnail ?>" />
+						<div>
+							<p class="subtitle"><?php echo $post_date ?></p>
+							<h3><?php echo $related_post->post_title ?></h3>
+							<p><?php echo $related_post->post_excerpt ?></p>
+						</div>
+					</a>
+					<?php
+				 }
+			}
+		?>
 </div>
+		</div>
 <?php get_footer(); ?>
